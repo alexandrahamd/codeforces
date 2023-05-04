3b7278083f6e7df4dd36edba67aeb18ab060e13d
alexandrahamd <pylina.alexandra@yandex.ru>
2023-05-04 14:33:29 +0300

    fix functions.py

--- functions.py
+++ functions.py
@@ -3,29 +3,32 @@ from crontab import CronTab
 
 
 def get_request_problems():
+    '''функция изъятия задач'''
     response = requests.get(f"https://codeforces.com/api/problemset.problems")
     result = response.json()['result']['problems']
     return result
 
 
 def get_request_statistics():
+    '''функция изъятия колличества решений'''
     response = requests.get(f"https://codeforces.com/api/problemset.problems")
     result = response.json()['result']['problemStatistics']
     return result
 
 
-def get_all_tags():
-    problems = get_request_problems()
-    all_tags = []
-
-    for item_problem in problems:
-        for tag in item_problem['tags']:
-            all_tags.append(tag)
-
-    return set(all_tags)
+# def get_all_tags():
+#     problems = get_request_problems()
+#     all_tags = []
+#
+#     for item_problem in problems:
+#         for tag in item_problem['tags']:
+#             all_tags.append(tag)
+#
+#     return set(all_tags)
 
 
 def get_data():
+    '''функия формирования данных'''
     data_problems = get_request_problems()
     data_statistics = get_request_statistics()
 
