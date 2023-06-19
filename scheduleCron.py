6152ba72629ca50195f93227199cd213ac7f97e1
alexandrahamd <43954475+alexandrahamd@users.noreply.github.com>
2023-06-19 12:31:59 +0300

    Update scheduleCron.py

--- scheduleCron.py
+++ scheduleCron.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 
 cron = CronTab(user='alexandra')
 
-job = cron.new(command='python example1.py', comment='comment')
+job = cron.new(command='python bd.py', comment='comment')
 job.hour.every(1)
 
 cron.write()
